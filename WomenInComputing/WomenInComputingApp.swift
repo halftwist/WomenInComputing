a0aaf6850dba5c20025c284b224e1b408d48e5f3
John Kearon <jkearon@hotmail.com>
2025-03-31 09:36:28 -0400

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by John Kearon on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
